1526858643b350cba918f694db2f37ce91425090
Maaj <maajb1122@gmail.com>
2025-12-05 12:28:27 +0530

    try to host

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -214,4 +215,6 @@ def home():
     }), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
